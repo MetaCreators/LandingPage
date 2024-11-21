8364ad4b113c3b4d97f44aa836748d780d507b49
Yash Hegde <yash4success@gmail.com>
2024-11-21 21:15:50 +0530

    hero section 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import HeroSection1 from "./components/HerSection1";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <HeroSection1 />
+      <HeroSection1 />
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
-    <div className="flex justify-between items-center px-10 py-4 sticky top-0 bg-sky-950 border border-b">
-      <div>Landing page</div>
+    <div className="flex justify-between items-center px-10 py-4 sticky top-0 bg-sky-950">
+      <div className="text-white">Landing page</div>
       <div className="flex items-center space-x-3 font-medium text-slate-500">
         <div>About</div>
         <div>Contact</div>
